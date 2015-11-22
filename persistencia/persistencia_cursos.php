8863a6474e657f738d922e0a056d636423adbfbe
aberaldo <andresbe1986@gmail.com>
2015-11-22 12:49:50 -0300

    obtener_info_cursos
    Finalizada la implementacion

--- persistencia/persistencia_cursos.php
+++ persistencia/persistencia_cursos.php
@@ -120,4 +120,78 @@ function guardar_creditos_validez($idCurso, $creditos, $validez){
 	}
 }
 
+function guardar_datos_aprobacion($idCurso, $datos){
+	
+	try {
+		
+		$conexion = null;
+		$conexion = conectar();
+		
+		$conexion->begin_transaction();
+		
+		//Chequeo si existe el curso
+		if(!existe_curso($idCurso)){
+			echo "<p style='padding: 5px;background-color: #FFC107;color: white;'>[guardar_datos_aprobacion]: El curso $idCurso no existe.</p>";
+			return;
+		}
+		
+		//Nota promedio
+		$success = $conexion->query("SELECT nota_promedio, total_cursantes, aprobados, exonerados, porcentaje_aprobacion FROM cursos WHERE idCurso = '$idCurso'");
+			
+		if(!$success){
+			throw new Exception("[guardar_datos_aprobacion]: Error al almacenar datos de aprobacion del curso $idCurso.");
+		}
+		
+		$query = $success->fetch_assoc();
+		if($datos['nota_promedio'] != 'S/N'){
+			$nota_promedio =  number_format(((float)$query['nota_promedio'] + (float)$datos['nota_promedio'])/2, 1, '.', '');
+		}else{
+			$nota_promedio = 'S/N';
+		}
+		
+		//Total cursantes
+		$total_cursantes = (int)$datos['total_cursantes'] + (int)$query['total_cursantes'];
+		
+		if(isset($datos['aprobados'])){
+			//Aprobados
+			$aprobados = (int)$datos['aprobados'] + (int)$query['aprobados'];
+		}
+	
+		//Exonerados
+		$exonerados = (int)$datos['exonerados'] + (int)$query['exonerados'];
+		
+		//Porcentaje de aprobacion
+		$porcentaje_aprobacion =  number_format(($exonerados/$total_cursantes)*100 , 1, '.', '');
+		
+		//Actualizo los datos
+		if(isset($datos['aprobados'])){
+			$success = $conexion->query("UPDATE cursos SET nota_promedio = '$nota_promedio', total_cursantes = $total_cursantes, aprobados = $aprobados, exonerados = $exonerados, porcentaje_aprobacion = $porcentaje_aprobacion WHERE idCurso = '$idCurso'");
+		}else{
+			$success = $conexion->query("UPDATE cursos SET nota_promedio = '$nota_promedio', total_cursantes = $total_cursantes, exonerados = $exonerados, porcentaje_aprobacion = $porcentaje_aprobacion WHERE idCurso = '$idCurso'");
+		}
+		
+		//Commit
+		$success = $conexion->commit();
+		
+		if(!$success){
+			throw new Exception("[guardar_datos_aprobacion]: Error al hacer commit curso $idCurso.");
+		}
+		
+		echo "<p style='padding: 5px;background-color: green;color: white;'>[guardar_datos_aprobacion]: Datos aprobacion de $idCurso han sido actualizado correctamente.</p>";
+			
+	}catch (Exception $e){
+		
+		if ($conexion != null)
+			$conexion->rollback();
+		
+		throw $e;
+		
+	} finally {
+		
+		if ($conexion != null)
+		$conexion->close();
+	
+	}
+}
+
 ?>
--- scrapping/obtener_info_cursos.php
+++ scrapping/obtener_info_cursos.php
@@ -40,7 +40,32 @@ function obtener_info_cursos($idCarrera){
 			}	
 		}
 	});
+	
+	//Obtengo porcentajes de aprobacion
+	$crawler = $client->request('GET', "http://www1.bedelias.edu.uy/ingenieria/actas_curso.sel_per_car");
 
+	//Obtengo los meses y obtengo los datos para cada periodo
+	$crawler->filter('select[name="p_permes"] option')->each(function ($node, $index) use ($crawler, $client, $idCarrera) {
+		$form = $crawler->selectButton('Aceptar')->form();
+		$actas_mes = $client->submit($form, array('p_permes' => $node->text(), "p_curri" => "C", "p_carrcic" => $idCarrera));
+		
+		//Por cada curso del periodo obtengo los datos
+		$actas_mes->filter('select[name="p_matnommat"] option')->each(function ($node, $index) use($actas_mes, $client){
+			
+			$form = $actas_mes->selectButton('Aceptar')->form();
+			$datos_curso = explode(" - ",$node->html());
+			$id_curso = trim($datos_curso[0]);
+			$acta_curso = $client->submit($form, array('p_matnommat' => $id_curso));
+			$datos = obtener_datos_aprobacion($acta_curso);
+			
+			try {
+				//guardar_datos_aprobacion($id_curso, $datos);
+			}catch (Exception $e){
+				echo $e->getMessage();
+			}	
+				
+		});
+	});
 }
 
  
@@ -100,6 +125,102 @@ function obtener_creditos_validez($fila){
 	$datos_curso[2] = $datos[7];
 	
 	return $datos_curso;
+}
+
+function obtener_datos_aprobacion($acta_curso) {
+	
+	$acta_curso = $acta_curso->filter('.unoa')->each(function($acta, $index){
+		
+		if($index == 0)
+			return;
+		
+		//Obtengo promedio
+		if($index == 1){
+			$notas = $acta->filter('tr td:nth-child(3)')->each(function($nota, $index){
+				if($index == 0)
+					return;
+				return (preg_replace('/[^\x00-\x7f£¢£€¥]+/u', '', $nota->text()));
+			});
+			
+			//Obtengo la suma total de notas y luego calculo el promedio
+			$suma_total = 0;
+	
+			foreach($notas as $nota){
+				if($suma_total != -1 and $nota != null){
+					
+					if($nota == ' S/N '){
+						$suma_total = -1;
+					}else{
+						$suma_total = $suma_total + (int)$nota;
+					}
+				}
+			}
+			
+			$cantidad_notas = count($notas)-1;
+			
+			if($suma_total != -1){
+				$promedio_notas = number_format($suma_total/$cantidad_notas, 1, '.', '');
+			}else{
+				$promedio_notas = 'S/N';
+			}
+			
+			return $promedio_notas;
+		}
+		
+		//Obtengo total y cantidad de aprobados
+		if($index == 2){
+		
+			$datos_aprobacion = $acta->filter('td:nth-child(2)')->each(function($totales, $indice){
+				
+				if($indice == 0){
+					$total = preg_replace('/[^\x00-\x7f£¢£€¥]+/u', '', $totales->text()); 
+					return $total;
+				}
+				
+				if($indice == 1){
+					$total = preg_replace('/[^\x00-\x7f£¢£€¥]+/u', '', $totales->text());
+					return $total;					
+				}
+				
+			});
+			
+			return $datos_aprobacion;
+		}
+		
+		//Detalle de aprobacion
+		if($index == 3){
+			
+			$datos_aprobacion = $acta->filter('td')->each(function($totales, $indice){
+				
+				if($indice == 2){
+					$total = preg_replace('/[^\x00-\x7f£¢£€¥]+/u', '', $totales->text()); 
+					return $total;
+				}
+				
+				if($indice == 4){
+					$total = preg_replace('/[^\x00-\x7f£¢£€¥]+/u', '', $totales->text()); 
+					return $total;
+				}
+				
+			});
+			
+			return $datos_aprobacion;
+		}
+	});
+	
+	//Armo array con los datos y retorno
+	$datos_cursos = array(
+		"nota_promedio"		=> trim($acta_curso[1]),
+		"total_cursantes"	=> trim($acta_curso[2][0]),
+		"exonerados"			=> trim($acta_curso[2][1]),
+	);
+	
+	if(isset($acta_curso[3])){
+		$datos_cursos["aprobados"] = $datos_cursos["exonerados"];
+		$datos_cursos["exonerados"] =  trim($acta_curso[3][4]);
+	}
+
+	return $datos_cursos;
 }
 
 ?>
